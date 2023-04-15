d17f7d7efaab612f339ca80d94116593a32d9a32
matheus <129121879+matheusimp@users.noreply.github.com>
2023-04-15 20:04:27 -0300

    Add a 'state' table and populate it with Brazilian states
    This will be used on the 'borrower' table

--- src/biblioteka/models.py
+++ src/biblioteka/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
+class CountryState(models.Model):
+    state = models.CharField("estado", max_length=100, unique=True)
+    abbreviation = models.CharField("sigla", max_length=2, unique=True)
