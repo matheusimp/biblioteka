b5379afd1b3029b6c97f97ed773b6850d9061d04
matheus <129121879+matheusimp@users.noreply.github.com>
2023-04-15 20:48:12 -0300

    Add 'borrower' table

--- src/biblioteka/models.py
+++ src/biblioteka/models.py
@@ -5,3 +5,17 @@ from django.db import models
 class CountryState(models.Model):
     state = models.CharField("estado", max_length=100, unique=True)
     abbreviation = models.CharField("sigla", max_length=2, unique=True)
+
+
+class Borrower(models.Model):
+    name = models.CharField("nome", max_length=255)
+    birthdate = models.DateField("data de nascimento")
+    cpf = models.CharField("CPF", max_length=11, unique=True)
+    phone = models.CharField("celular", max_length=20)
+    email = models.EmailField("email", blank=True, default=None, null=True)
+    zip_code = models.CharField("cep", max_length=8)
+    address = models.CharField("endereço", max_length=255)
+    city = models.CharField("cidade", max_length=100)
+    state = models.ForeignKey(
+        CountryState, on_delete=models.PROTECT, verbose_name="estado"
+    )
