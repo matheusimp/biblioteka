2c8c9185d867b560d12f3e988d32fe54c9988670
matheus <129121879+matheusimp@users.noreply.github.com>
2023-04-16 00:07:48 -0300

    Add form to register new borrowers and handle the request on backend

--- src/biblioteka/urls.py
+++ src/biblioteka/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = "biblioteka"
 
 urlpatterns = [
+    path("borrowers/", views.borrowers, name="borrowers"),
+    path("borrowers/new/", views.borrowers_new, name="borrowers_new"),
     path("", views.index, name="index"),
 ]
