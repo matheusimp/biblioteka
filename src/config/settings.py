89ed7cf8bb17f0cb98ff203af5aed8aa84504d11
matheus <129121879+matheusimp@users.noreply.github.com>
2023-05-19 22:48:54 -0300

    Get CSRF_TRUSTED_ORIGINS from env variable

--- src/config/settings.py
+++ src/config/settings.py
@@ -59,6 +59,8 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS", "").split(" ")
+
 ROOT_URLCONF = "config.urls"
 
 TEMPLATES = [
